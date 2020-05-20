0a5176f07ff6d80cec5f16b0b4a51a8ca4024818
陈秀红 <xiuhong.chen@hand-china.com>
2020-05-20 20:52:37 +0800

    [FIX]误改

--- init-database.sh
+++ init-database.sh
@@ -5,11 +5,11 @@ then
     curl https://nexus.choerodon.com.cn/repository/choerodon-release/io/choerodon/choerodon-tool-liquibase/0.9.2.RELEASE/choerodon-tool-liquibase-0.9.2.RELEASE.jar -o target/choerodon-tool-liquibase.jar
 fi
 
-java -Dspring.datasource.url="jdbc:mysql://118.25.175.161:3306/hrds_doc_repo_2?useUnicode=true&characterEncoding=utf-8&useSSL=false" \
-	 -Dspring.datasource.username=admin \
-	 -Dspring.datasource.password=Admin@1234 \
+java -Dspring.datasource.url="jdbc:mysql://db.hzero.org:3306/hzero_platform?useUnicode=true&characterEncoding=utf-8&useSSL=false" \
+	 -Dspring.datasource.username=hzero \
+	 -Dspring.datasource.password=hzero \
 	 -Ddata.drop=false -Ddata.init=init \
-	 -Ddata.dir=src/main/resources/script/test \
+	 -Ddata.dir=src/main/resources \
 	 -jar target/choerodon-tool-liquibase.jar
 
 
