eefdf998f8346093cc6ed1279276da17773611ce
limofei <1458908533@qq.com>
2020-05-27 14:26:23 +0800

    [ADD]:nexus组织层日志查询接口

--- src/main/java/org/hrds/rdupm/nexus/app/service/impl/NexusLogServiceImpl.java
+++ src/main/java/org/hrds/rdupm/nexus/app/service/impl/NexusLogServiceImpl.java
@@ -1,6 +1,25 @@
 package org.hrds.rdupm.nexus.app.service.impl;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import io.choerodon.core.domain.Page;
+import io.choerodon.mybatis.pagehelper.domain.PageRequest;
+import org.apache.commons.collections.CollectionUtils;
+import org.hrds.rdupm.harbor.infra.feign.BaseFeignClient;
+import org.hrds.rdupm.harbor.infra.feign.dto.ProjectDTO;
 import org.hrds.rdupm.nexus.app.service.NexusLogService;
+import org.hrds.rdupm.nexus.domain.entity.NexusLog;
+import org.hrds.rdupm.nexus.domain.repository.NexusLogRepository;
+import org.hrds.rdupm.nexus.infra.feign.BaseServiceFeignClient;
+import org.hrds.rdupm.nexus.infra.feign.vo.ProjectVO;
+import org.hrds.rdupm.nexus.infra.mapper.NexusLogMapper;
+import org.hrds.rdupm.nexus.infra.util.PageConvertUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 /**
  * 制品库_nexus日志表应用服务默认实现
@@ -10,4 +29,30 @@ import org.springframework.stereotype.Service;
 @Service
 public class NexusLogServiceImpl implements NexusLogService {
 
+    @Autowired
+    private BaseServiceFeignClient baseServiceFeignClient;
+    @Autowired
+    private NexusLogRepository nexusLogRepository;
+    @Autowired
+    private NexusLogMapper nexusLogMapper;
+
+    @Override
+    public Page<NexusLog> listLogByOrg(Long organizationId, String repoType, Long projectId, String neRepositoryName, String loginName, String operateType, Date startDate, Date endDate, PageRequest pageRequest) {
+        List<NexusLog> nexusLogList = nexusLogMapper.listNpmLogByOrg(organizationId, repoType, projectId, neRepositoryName, loginName, operateType, startDate, endDate);
+
+        Set<Long> projectIdSet = nexusLogList.stream().map(NexusLog::getProjectId).collect(Collectors.toSet());
+        List<ProjectVO> projectVOList = baseServiceFeignClient.queryByIds(projectIdSet);
+        Map<Long, List<ProjectVO>> projectDataMap = projectVOList.stream().collect(Collectors.groupingBy(ProjectVO::getId));
+        for (NexusLog log : nexusLogList
+             ) {
+            List<ProjectVO> projectVOS = projectDataMap.get(log.getProjectId());
+            if (CollectionUtils.isNotEmpty(projectVOS)) {
+                ProjectVO projectVO = projectVOS.get(0);
+                log.setProjectCode(projectVO.getCode());
+                log.setProjectName(projectVO.getName());
+                log.setProjectImageUrl(projectVO.getImageUrl());
+            }
+        }
+        return PageConvertUtils.convert(pageRequest.getPage(),pageRequest.getSize(),nexusLogList);
+    }
 }
--- src/main/java/org/hrds/rdupm/nexus/infra/mapper/NexusLogMapper.java
+++ src/main/java/org/hrds/rdupm/nexus/infra/mapper/NexusLogMapper.java
@@ -1,5 +1,9 @@
 package org.hrds.rdupm.nexus.infra.mapper;
 
+import java.util.Date;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
 import org.hrds.rdupm.nexus.domain.entity.NexusLog;
 import io.choerodon.mybatis.common.BaseMapper;
 
@@ -9,5 +13,20 @@ import io.choerodon.mybatis.common.BaseMapper;
  * @author weisen.yang@hand-china.com 2020-05-26 22:55:13
  */
 public interface NexusLogMapper extends BaseMapper<NexusLog> {
+    /**
+     * 组织层-NPM操作日志列表
+     *
+     * @param organizationId
+     * @param repoType
+     * @param projectId
+     * @param neRepositoryName
+     * @param loginName
+     * @param operateType
+     * @param startDate
+     * @param endDate
+     * @return List<NexusLog>
+     */
+    List<NexusLog> listNpmLogByOrg(@Param("organizationId") Long organizationId, @Param("repoType") String repoType, @Param("projectId") Long projectId, @Param("neRepositoryName") String neRepositoryName,
+                                   @Param("loginName") String loginName, @Param("operateType") String operateType, @Param("startDate") Date startDate, @Param("endDate") Date endDate);
 
 }
