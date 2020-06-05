5e753c9dddc31a8988cdae4bbbb0a92b7528273f
weisen.yang <weisen.yang@hand-china.com>
2020-06-05 17:54:04 +0800

    [FIX] 仓库更新

--- src/main/resources/script/nexus/create_npm_hosted.groovy
+++ src/main/resources/script/nexus/create_npm_hosted.groovy
@@ -16,7 +16,7 @@ Repository existingRepository = repositoryManager.get(param.name)
 if (existingRepository != null) {
 
   newConfig = existingRepository.configuration.copy()
-  newConfig.attributes['storage']['writePolicy'] = param.storage.versionPolicy.toUpperCase()
+  newConfig.attributes['storage']['writePolicy'] = param.storage.writePolicy.toUpperCase()
   repositoryManager.update(newConfig)
 } else {
   WritePolicy writePolicy = Enum.valueOf(WritePolicy.class, param.storage.writePolicy.toUpperCase())
