167bdcdcfdaf6cd1adc08b3d1f7fbacca7649fbb
张理科 <like.zhang@hand-china.com>
2020-06-15 22:01:55 +0800

    [FIX] 修改读取逻辑

--- src/main/java/org/hrds/rdupm/util/XMLValidator.java
+++ src/main/java/org/hrds/rdupm/util/XMLValidator.java
@@ -1,12 +1,15 @@
 package org.hrds.rdupm.util;
 
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.util.ResourceUtils;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.multipart.MultipartFile;
 import org.xml.sax.SAXException;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import javax.xml.XMLConstants;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
@@ -30,8 +33,8 @@ public class XMLValidator {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(XMLValidator.class);
 
-    private static final String POM_XSD_DEFAULT_PATH = "classpath:xsd/pom0.xsd";
-    private static final String POM_XSD_DEFAULT_PATH_1 = "classpath:xsd/pom1.xsd";
+    private static final String POM_XSD_DEFAULT_PATH = "xsd/pom0.xsd";
+    private static final String POM_XSD_DEFAULT_PATH_1 = "xsd/pom1.xsd";
 
     public static void validXMLDefault(MultipartFile xml) {
         XMLValidator.validXml(xml, POM_XSD_DEFAULT_PATH, POM_XSD_DEFAULT_PATH_1);
@@ -44,10 +47,12 @@ public class XMLValidator {
 
             for (String xsdPath : xsdPaths) {
                 SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-                Schema schema = factory.newSchema(ResourceUtils.getFile(xsdPath));
+                File xsdFile = getFile(xsdPath);
+                Schema schema = factory.newSchema(xsdFile);
                 Validator validator = schema.newValidator();
                 // 校验pom文件结构
                 validator.validate(new StreamSource(xml.getInputStream()));
+                xsdFile.deleteOnExit();
             }
 
             SAXHandler saxHandler = new SAXHandler();
@@ -74,10 +79,12 @@ public class XMLValidator {
 
             for (String xsdPath : xsdPaths) {
                 SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-                Schema schema = factory.newSchema(ResourceUtils.getFile(xsdPath));
+                File xsdFile = getFile(xsdPath);
+                Schema schema = factory.newSchema(xsdFile);
                 Validator validator = schema.newValidator();
                 // 校验pom文件结构
                 validator.validate(new StreamSource(new File(xmlPath)));
+                xsdFile.deleteOnExit();
             }
 
             SAXHandler saxHandler = new SAXHandler();
@@ -91,5 +98,13 @@ public class XMLValidator {
         } catch (Exception e) {
             throw new CommonException(e);
         }
+    }
+
+    private static File getFile(String filePath) throws IOException {
+        ClassPathResource resource = new ClassPathResource(filePath);
+        InputStream is = resource.getInputStream();
+        File tempFile = new File("temp.xml");
+        FileUtils.copyInputStreamToFile(is, tempFile);
+        return tempFile;
     }
 }
