51fb63aafe5c8b674deb8b984c73ee021b994f41
weisen.yang <weisen.yang@hand-china.com>
2020-09-28 17:44:18 +0800

    [FIX] CI

--- src/main/java/org/hrds/rdupm/RdupmApplication.java
+++ src/main/java/org/hrds/rdupm/RdupmApplication.java
@@ -18,7 +18,6 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @SpringBootApplication
 public class RdupmApplication {
 
-
     public static void main(String[] args) {
         SpringApplication.run(RdupmApplication.class, args);
     }
