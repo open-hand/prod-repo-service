6c67da4caa7ae58bdbab9626f76f3a223732e8d5
陈秀红 <xiuhong.chen@hand-china.com>
2020-10-22 19:33:53 +0800

    [ADD]批量关联仓库、两次密码重复、修改密码空指针

--- src/main/java/org/hrds/rdupm/harbor/infra/repository/impl/HarborRepoServiceRepositoryImpl.java
+++ src/main/java/org/hrds/rdupm/harbor/infra/repository/impl/HarborRepoServiceRepositoryImpl.java
@@ -1,8 +1,13 @@
 package org.hrds.rdupm.harbor.infra.repository.impl;
 
+import java.util.List;
+import javax.annotation.Resource;
+
+import org.hrds.rdupm.harbor.infra.mapper.HarborRepoServiceMapper;
 import org.hzero.mybatis.base.impl.BaseRepositoryImpl;
 import org.hrds.rdupm.harbor.domain.entity.HarborRepoService;
 import org.hrds.rdupm.harbor.domain.repository.HarborRepoServiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,5 +18,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class HarborRepoServiceRepositoryImpl extends BaseRepositoryImpl<HarborRepoService> implements HarborRepoServiceRepository {
 
-  
+    @Resource
+    private HarborRepoServiceMapper harborRepoServiceMapper;
+
+    @Override
+    public void deleteRelationByProjectId(Long projectId) {
+        harborRepoServiceMapper.deleteRelationByProjectId(projectId);
+    }
+
+    @Override
+    public void updateProjectShareByProjectId(Long projectId, Boolean projectShare, Long repoId) {
+        harborRepoServiceMapper.updateProjectShareByProjectId(projectId,projectShare.toString(),repoId);
+    }
+
+    @Override
+    public void deleteOtherRelationByService(Long projectId, List<Long> appServiceIds, Long repoId) {
+        harborRepoServiceMapper.deleteOtherRelationByService(projectId,appServiceIds,repoId);
+    }
 }
