38090b5e621979c452a27795e37c0d4f98134247
weisen.yang <weisen.yang@hand-china.com>
2020-10-23 15:38:48 +0800

    [FIX] 初始返回

--- src/main/java/org/hrds/rdupm/nexus/api/dto/NexusInitErrorDTO.java
+++ src/main/java/org/hrds/rdupm/nexus/api/dto/NexusInitErrorDTO.java
@@ -1,18 +1,16 @@
 package org.hrds.rdupm.nexus.api.dto;
 
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotBlank;
 
 /**
  * 描述
  *
  * @author weisen.yang@hand-china.com 2020/10/23
  */
-@ApiModel("nexus maven-仓库配置")
+@ApiModel("错误返回")
 @Getter
 @Setter
 public class NexusInitErrorDTO {
