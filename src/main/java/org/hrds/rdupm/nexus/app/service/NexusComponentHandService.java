e164c4f3dd7cb5ab39e5ec9e0cf8399499c57404
wangxiang <xiangwang04@hand-china.com>
2021-01-05 09:21:18 +0800

    [REF] 增加异步方法
    [FIX] 修改pom

---
+++ src/main/java/org/hrds/rdupm/nexus/app/service/NexusComponentHandService.java
@@ -0,0 +1,13 @@
+package org.hrds.rdupm.nexus.app.service;
+
+import org.hrds.rdupm.nexus.client.nexus.NexusClient;
+import org.hrds.rdupm.nexus.client.nexus.model.NexusServer;
+import org.hrds.rdupm.nexus.client.nexus.model.NexusServerComponentUpload;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Created by wangxiang on 2021/1/4
+ */
+public interface NexusComponentHandService {
+    void uploadJar(NexusClient nexusClient, MultipartFile multipartFile, MultipartFile assetPom, NexusServerComponentUpload nexusServerComponentUpload, NexusServer currentNexusServer);
+}
