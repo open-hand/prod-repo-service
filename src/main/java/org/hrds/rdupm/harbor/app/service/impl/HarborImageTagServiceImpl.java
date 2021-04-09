fcbe590877871fe4bfb224a3713bd698cece0951
18986 <changping.shi@hand-chian.com>
2021-04-09 10:29:08 +0800

    [IMP] 修改查询推送者信息

--- src/main/java/org/hrds/rdupm/harbor/app/service/impl/HarborImageTagServiceImpl.java
+++ src/main/java/org/hrds/rdupm/harbor/app/service/impl/HarborImageTagServiceImpl.java
@@ -96,19 +96,22 @@ public class HarborImageTagServiceImpl implements HarborImageTagService {
 
 	public void setAuthorWithIam(List<HarborImageTagVo> harborImageTagVoList) {
 		Set<String> userNameSet = new HashSet<>();
-		harborImageTagVoList.forEach(dto -> dto.getTags().forEach(tag -> userNameSet.add(tag.getAuthor())));
-		Map<String,UserDTO> userDtoMap = c7nBaseService.listUsersByLoginNames(userNameSet);
-		harborImageTagVoList.forEach(dto->{
-			dto.getTags().forEach(tag -> {
-				String loginName = tag.getAuthor();
-				UserDTO userDTO = userDtoMap.get(loginName);
-				String realName = userDTO == null ? loginName : userDTO.getRealName();
-				String userImageUrl = userDTO == null ? null : userDTO.getImageUrl();
-				tag.setLoginName(loginName);
-				tag.setRealName(realName);
-				tag.setUserImageUrl(userImageUrl);
+		List<HarborImageTagVo> harborImageTagVos = harborImageTagVoList.stream().filter(dto -> CollectionUtils.isNotEmpty(dto.getTags())).collect(Collectors.toList());
+		if (CollectionUtils.isNotEmpty(harborImageTagVos)) {
+			harborImageTagVos.forEach(dto -> dto.getTags().forEach(tag -> userNameSet.add(tag.getAuthor())));
+			Map<String, UserDTO> userDtoMap = c7nBaseService.listUsersByLoginNames(userNameSet);
+			harborImageTagVoList.forEach(dto -> {
+				dto.getTags().forEach(tag -> {
+					String loginName = tag.getAuthor();
+					UserDTO userDTO = userDtoMap.get(loginName);
+					String realName = userDTO == null ? loginName : userDTO.getRealName();
+					String userImageUrl = userDTO == null ? null : userDTO.getImageUrl();
+					tag.setLoginName(loginName);
+					tag.setRealName(realName);
+					tag.setUserImageUrl(userImageUrl);
+				});
 			});
-		});
+		}
 	}
 
 	@Override
