913bef31b1143ba6c86507bf9cd3f97b8ecb62c8
wangxiang <xiangwang04@hand-china.com>
2021-04-30 17:04:54 +0800

    [FIX] 校验仓库重名

--- src/main/java/org/hrds/rdupm/harbor/infra/repository/impl/HarborRepositoryRepositoryImpl.java
+++ src/main/java/org/hrds/rdupm/harbor/infra/repository/impl/HarborRepositoryRepositoryImpl.java
@@ -1,6 +1,8 @@
 package org.hrds.rdupm.harbor.infra.repository.impl;
 
 import io.choerodon.core.oauth.DetailsHelper;
+
+import java.util.Objects;
 import org.hzero.mybatis.base.impl.BaseRepositoryImpl;
 import org.hrds.rdupm.harbor.domain.entity.HarborRepository;
 import org.hrds.rdupm.harbor.domain.repository.HarborRepositoryRepository;
@@ -16,23 +18,35 @@ import org.springframework.stereotype.Component;
 @Component
 public class HarborRepositoryRepositoryImpl extends BaseRepositoryImpl<HarborRepository> implements HarborRepositoryRepository {
 
-	@Override
-	public HarborRepository getHarborRepositoryById(Long projectId){
-		// todo scp 上线之前删除
+    @Override
+    public HarborRepository getHarborRepositoryById(Long projectId) {
+        // todo scp 上线之前删除
 //		Long organizationId = DetailsHelper.getUserDetails().getTenantId();
-		HarborRepository harborRepository = this.selectByCondition(Condition.builder(HarborRepository.class).where(Sqls.custom()
+        HarborRepository harborRepository = this.selectByCondition(Condition.builder(HarborRepository.class).where(Sqls.custom()
 //				.andEqualTo(HarborRepository.FIELD_ORGANIZATION_ID,organizationId)
-				.andEqualTo(HarborRepository.FIELD_PROJECT_ID,projectId)
-		).build()).stream().findFirst().orElse(null);
-		return harborRepository;
-	}
+                        .andEqualTo(HarborRepository.FIELD_PROJECT_ID, projectId)
+        ).build()).stream().findFirst().orElse(null);
+        return harborRepository;
+    }
 
-	@Override
-	public HarborRepository getHarborRepositoryByHarborId(Long harborId){
-		HarborRepository harborRepository = this.selectByCondition(Condition.builder(HarborRepository.class).where(Sqls.custom()
-				.andEqualTo(HarborRepository.FIELD_HARBOR_ID,harborId)
-		).build()).stream().findFirst().orElse(null);
-		return harborRepository;
-	}
+    @Override
+    public HarborRepository getHarborRepositoryByHarborId(Long harborId) {
+        HarborRepository harborRepository = this.selectByCondition(Condition.builder(HarborRepository.class).where(Sqls.custom()
+                .andEqualTo(HarborRepository.FIELD_HARBOR_ID, harborId)
+        ).build()).stream().findFirst().orElse(null);
+        return harborRepository;
+    }
 
+    @Override
+    public Boolean checkName(Long projectId, String repositoryName) {
+        HarborRepository harborRepository = this.selectByCondition(Condition.builder(HarborRepository.class).where(Sqls.custom()
+                .andEqualTo(HarborRepository.FIELD_PROJECT_ID, projectId)
+                .andEqualTo(HarborRepository.FIELD_CODE, repositoryName)
+        ).build()).stream().findFirst().orElse(null);
+        if (Objects.isNull(harborRepository)) {
+            return Boolean.TRUE;
+        } else {
+            return Boolean.FALSE;
+        }
+    }
 }
