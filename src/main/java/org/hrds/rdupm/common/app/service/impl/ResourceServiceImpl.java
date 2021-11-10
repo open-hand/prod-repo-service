afc8d221f360f518d5950330d98001ab96355352
wangxiang <xiangwang04@hand-china.com>
2021-11-10 14:07:55 +0800

    [FIX] 加上单位

--- src/main/java/org/hrds/rdupm/common/app/service/impl/ResourceServiceImpl.java
+++ src/main/java/org/hrds/rdupm/common/app/service/impl/ResourceServiceImpl.java
@@ -9,9 +9,7 @@ import org.hrds.rdupm.common.app.service.ResourceService;
 import org.hrds.rdupm.harbor.api.vo.HarborQuotaVo;
 import org.hrds.rdupm.harbor.app.service.HarborQuotaService;
 import org.hrds.rdupm.harbor.domain.entity.HarborRepository;
-import org.hrds.rdupm.harbor.infra.constant.HarborConstants;
 import org.hrds.rdupm.harbor.infra.mapper.HarborRepositoryMapper;
-import org.hrds.rdupm.harbor.infra.util.HarborUtil;
 import org.hrds.rdupm.nexus.api.vo.ResourceVO;
 import org.hrds.rdupm.nexus.domain.entity.NexusAssets;
 import org.hrds.rdupm.nexus.infra.mapper.NexusAssetsMapper;
@@ -23,6 +21,8 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class ResourceServiceImpl implements ResourceService {
+
+    private static final Long ONE_GB_TO_B = 1073741824L;
 
     @Autowired
     private HarborQuotaService harborQuotaService;
@@ -57,14 +57,23 @@ public class ResourceServiceImpl implements ResourceService {
             DecimalFormat df = new DecimalFormat("#.00");
             HarborQuotaVo harborQuotaVo = harborQuotaService.getProjectQuota(harborRepository.getProjectId());
             if (harborQuotaVo != null) {
-                resourceVO.setCurrentHarborCapacity(df.format(Math.pow(harborQuotaVo.getUsedStorage(), 1.0 / 3.0)));
+                if (harborQuotaVo.getUsedStorage() < ONE_GB_TO_B) {
+                    resourceVO.setCurrentHarborCapacity(df.format(Math.pow(harborQuotaVo.getUsedStorage(), 1.0 / 2.0)) + "MB");
+
+                } else if (harborQuotaVo.getUsedStorage() >= ONE_GB_TO_B) {
+                    resourceVO.setCurrentHarborCapacity(df.format(Math.pow(harborQuotaVo.getUsedStorage(), 1.0 / 3.0) + "GB"));
+                }
             }
             NexusAssets assetRecord = new NexusAssets();
             assetRecord.setProjectId(projectId);
             List<NexusAssets> nexusAssets = nexusAssetsMapper.select(assetRecord);
             if (!CollectionUtils.isEmpty(nexusAssets)) {
                 long count = nexusAssets.stream().map(NexusAssets::getSize).count();
-                resourceVO.setCurrentNexusCapacity(df.format(Math.pow(count, 1.0 / 3.0)));
+                if (count < ONE_GB_TO_B) {
+                    resourceVO.setCurrentNexusCapacity(df.format(Math.pow(count, 1.0 / 2.0)) + "MB");
+                } else if (count >= ONE_GB_TO_B) {
+                    resourceVO.setCurrentNexusCapacity(df.format(Math.pow(count, 1.0 / 3.0) + "GB"));
+                }
             }
             result.add(resourceVO);
 
